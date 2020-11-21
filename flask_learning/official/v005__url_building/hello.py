2694bcc1d260f172c2aa2d37223a8e85ce162e48
Derek Wan <d.wan@icloud.com>
2020-11-21 15:00:14 +0800

    Add tests of v005 -- URL building

--- flask_learning/official/v005__url_building/hello.py
+++ flask_learning/official/v005__url_building/hello.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from flask import Flask
-from flask import url_for
 from markupsafe import escape
 
 
@@ -21,11 +20,3 @@ def login() -> str:
 @app.route("/user/<username>")
 def profile(username: str) -> str:
     return f"{escape(username)}'s profile"
-
-
-if __name__ == "__main__":
-    with app.test_request_context():
-        print(url_for("index"))
-        print(url_for("login"))
-        print(url_for("login", next="/"))
-        print(url_for("profile", username="John Doe"))
---
+++ tests/official/v005__url_building/test_hello.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from flask import url_for
+
+from flask_learning.official.v005__url_building.hello import app
+
+
+def test_hello() -> None:
+    with app.test_request_context():
+        assert url_for("index") == "/"
+        assert url_for("login") == "/login"
+        assert url_for("login", next="/") == "/login?next=%2F"
+        assert url_for("profile", username="John Doe") == "/user/John%20Doe"
