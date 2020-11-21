13c0f06d8dddaa3138c9e9476a282ece9d446bab
Derek Wan <d.wan@icloud.com>
2020-11-21 20:11:52 +0800

    Build flaskr, as per the tutorial (#3)

---
+++ flaskr/auth.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+import functools
+from typing import Any
+from typing import Callable
+from typing import Union
+
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+from werkzeug import Response
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+
+from flaskr.db import get_db
+
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register() -> Union[str, Response]:
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        elif (
+            db.execute(
+                "SELECT id FROM user WHERE username = ?",
+                (username,),
+            ).fetchone()
+            is not None
+        ):
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login() -> Union[str, Response]:
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?",
+            (username,),
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
+@bp.before_app_request
+def load_logged_in_user() -> None:
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db()
+            .execute(
+                "SELECT * FROM user WHERE id = ?",
+                (user_id,),
+            )
+            .fetchone()
+        )
+
+
+@bp.route("/logout")
+def logout() -> Response:
+    session.clear()
+    return redirect(url_for("index"))
+
+
+def login_required(view: Callable) -> Callable:
+    @functools.wraps(view)
+    def wrapped_view(**kwargs: Any) -> Union[str, Response]:
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
---
+++ tests/test_auth.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from flask import Flask
+from flask import g
+from flask import session
+from flask.testing import FlaskClient
+from pytest import mark
+
+from flaskr.db import get_db
+from tests.conftest import AuthActions
+
+
+def test_register(client: FlaskClient, app: Flask) -> None:
+    assert client.get("/auth/register").status_code == 200
+    response = client.post(
+        "/auth/register",
+        data={"username": "a", "password": "a"},
+    )
+    assert "http://localhost/auth/login" == response.headers["Location"]
+
+    with app.app_context():
+        assert (
+            get_db()
+            .execute(
+                "select * from user where username = 'a'",
+            )
+            .fetchone()
+            is not None
+        )
+
+
+@mark.parametrize(  # type: ignore
+    ("username", "password", "message"),
+    (
+        ("", "", b"Username is required."),
+        ("a", "", b"Password is required."),
+        ("test", "test", b"already registered"),
+    ),
+)
+def test_register_validate_input(
+    client: FlaskClient,
+    username: str,
+    password: str,
+    message: bytes,
+) -> None:
+    response = client.post(
+        "/auth/register",
+        data={"username": username, "password": password},
+    )
+    assert message in response.data
+
+
+def test_login(client: FlaskClient, auth: AuthActions) -> None:
+    assert client.get("/auth/login").status_code == 200
+    response = auth.login()
+    assert response.headers["Location"] == "http://localhost/"
+
+    with client:
+        client.get("/")
+        assert session["user_id"] == 1
+        assert g.user["username"] == "test"
+
+
+@mark.parametrize(  # type: ignore
+    ("username", "password", "message"),
+    (
+        ("a", "test", b"Incorrect username."),
+        ("test", "a", b"Incorrect password."),
+    ),
+)
+def test_login_validate_input(
+    auth: AuthActions,
+    username: str,
+    password: str,
+    message: bytes,
+) -> None:
+    response = auth.login(username, password)
+    assert message in response.data
+
+
+def test_logout(client: FlaskClient, auth: AuthActions) -> None:
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert "user_id" not in session
