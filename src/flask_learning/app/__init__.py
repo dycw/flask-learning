87147934d4b3ab429b9267acfe89bcec56521aa3
Derek Wan <d.wan@icloud.com>
2021-03-07 21:01:26 +0800

    Add config to the app

--- src/flask_learning/app/__init__.py
+++ src/flask_learning/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
+from flask_learning.config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 from flask_learning.app import routes  # noqa: E402, F401
