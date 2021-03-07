5f94e6281219bdc5d2653938213f33f99c942f4a
Derek Wan <d.wan@icloud.com>
2021-03-07 20:02:54 +0800

    Add conditional to index.html title

--- src/flask_learning/app/routes.py
+++ src/flask_learning/app/routes.py
@@ -1,10 +1,16 @@
+from random import SystemRandom
+
 from flask import render_template
 
 from flask_learning.app import app
 
 
+RANDOM = SystemRandom()
+
+
 @app.route("/")
 @app.route("/index")
 def index() -> str:
+    title = "Home" if RANDOM.uniform(0.0, 1.0) <= 0.5 else None
     user = {"username": "Derek"}
-    return render_template("index.html", title="Home", user=user)
+    return render_template("index.html", title=title, user=user)
