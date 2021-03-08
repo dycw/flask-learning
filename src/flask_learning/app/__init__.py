bac5c4df8ea72e97769e24c6738bff0c9237c58d
Derek Wan <d.wan@icloud.com>
2021-03-08 22:22:36 +0800

    Update flake8

--- src/flask_learning/app/__init__.py
+++ src/flask_learning/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from flask_learning.config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
+from flask_learning.app import models  # noqa: E402, F401
 from flask_learning.app import routes  # noqa: E402, F401
