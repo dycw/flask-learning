2f90b5e47025a1214f1121c70278488b30291396
Derek Wan <d.wan@icloud.com>
2021-03-08 22:02:08 +0800

    Use url_for (#18)
    * Use url_for
    
    * whitespace
    
    * Use url_for

--- src/flask_learning/app/routes.py
+++ src/flask_learning/app/routes.py
@@ -4,6 +4,7 @@ from typing import Union
 from flask import flash
 from flask import redirect
 from flask import render_template
+from flask import url_for
 from werkzeug import Response
 
 from flask_learning.app import app
@@ -36,6 +37,6 @@ def login() -> Union[str, Response]:
             f"Login requested for user {form.username.data}, "
             f"remember_me={form.remember_me.data}"
         )
-        return redirect("/index")
+        return redirect(url_for("index"))
     else:
         return render_template("login.html", title="Sign In", form=form)
