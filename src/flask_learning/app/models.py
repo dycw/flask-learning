aa6035cf0253ed0d46cf5cb68c1c9ee37562ed23
Derek Wan <d.wan@icloud.com>
2021-03-09 22:04:47 +0800

    Add Post table (#23)
    * Update pre-commit hooks
    
    * Start working on typing the DbColumn, etc
    
    * Add more typing
    
    * Remove pyright ignore
    
    * Add posts table
    
    * Upgrade db

--- src/flask_learning/app/models.py
+++ src/flask_learning/app/models.py
@@ -1,11 +1,40 @@
+import datetime as dt
+from typing import cast
+
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
 from flask_learning.app import db
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+DbColumn = cast(type[Column], db.Column)  # type: ignore
+DbDateTime = cast(type[DateTime], db.DateTime)  # type: ignore
+DbInteger = cast(type[Integer], db.Integer)  # type: ignore
+DbString = cast(type[String], db.String)  # type: ignore
+DbForeignKey = cast(type[ForeignKey], db.ForeignKey)  # type: ignore
+DbRelationship = cast(type[relationship], db.relationship)  # type: ignore
+
+
+class User(db.Model):  # type: ignore
+    id = DbColumn(DbInteger, primary_key=True)
+    username = DbColumn(DbString(64), index=True, unique=True)
+    email = DbColumn(DbString(120), index=True, unique=True)
+    password_hash = DbColumn(DbString(128))
+    posts = DbRelationship("Post", backref="author")
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
+
+
+class Post(db.Model):  # type: ignore
+    id = DbColumn(DbInteger, primary_key=True)
+    body = DbColumn(DbString(140))
+    timestamp = DbColumn(DbDateTime, index=True, default=dt.datetime.utcnow)
+    user_id = DbColumn(DbInteger, DbForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"<Post {self.body}>"
