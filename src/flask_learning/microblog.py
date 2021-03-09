72fb7754b22c76918fa7483a8b5d37e9b641883a
Derek Wan <d.wan@icloud.com>
2021-03-09 22:29:09 +0800

    Add shell_context_processor in microblog.py, fix FLASK_APP

--- src/flask_learning/microblog.py
+++ src/flask_learning/microblog.py
@@ -1,1 +1,11 @@
-from flask_learning.app import app  # noqa: F401
+from typing import Any
+
+from flask_learning.app import app
+from flask_learning.app import db
+from flask_learning.app.models import Post
+from flask_learning.app.models import User
+
+
+@app.shell_context_processor
+def make_shell_context() -> dict[str, Any]:
+    return {"db": db, "User": User, "Post": Post}
