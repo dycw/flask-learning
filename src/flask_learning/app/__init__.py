d19b92300ee4a99468e9221d972d4db1319ea042
Derek Wan <d.wan@icloud.com>
2021-03-13 14:39:08 +0800

    Update

--- src/flask_learning/app/__init__.py
+++ src/flask_learning/app/__init__.py
@@ -1,3 +1,4 @@
+from logging import ERROR
 from typing import cast
 
 from flask import Flask
@@ -5,6 +6,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Session
+from ssl_smtp_handler import SSLSMTPHandler
 
 from flask_learning.config import Config
 
@@ -21,3 +23,16 @@ login.login_view = "login"  # type: ignore
 from flask_learning.app import errors  # noqa: E402, F401
 from flask_learning.app import models  # noqa: E402, F401
 from flask_learning.app import routes  # noqa: E402, F401
+
+
+if not app.debug and (mail_host := app.config["MAIL_HOST"]):
+    mail_username = app.config["MAIL_USERNAME"]
+    handler = SSLSMTPHandler(
+        mailhost=mail_host,
+        fromaddr=mail_username,
+        toaddrs=[app.config["MAIL_TO_ADDRESS"]],
+        subject="Microblog failure",
+        credentials=(mail_username, app.config["MAIL_PASSWORD"]),
+    )
+    handler.setLevel(ERROR)
+    app.logger.addHandler(handler)
