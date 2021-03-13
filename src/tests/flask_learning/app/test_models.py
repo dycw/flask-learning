9d2c6677b131b5740680ce301808d8d3d6b6d225
Derek Wan <d.wan@icloud.com>
2021-03-13 20:49:10 +0800

    Add test of avatar (#57)

--- src/tests/flask_learning/app/test_models.py
+++ src/tests/flask_learning/app/test_models.py
@@ -7,3 +7,10 @@ class TestUser:
         user.set_password("cat")
         assert user.check_password("cat")
         assert not user.check_password("dog")
+
+    def test_avatar(self) -> None:
+        user = User(username="john", email="john@example.com")
+        assert user.avatar(128) == (
+            "https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6"
+            "?d=identicon&s=128"
+        )
