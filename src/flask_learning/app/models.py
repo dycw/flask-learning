9e1cea8801d639ac5e089b60ac426dafaafe59b2
Derek Wan <d.wan@icloud.com>
2021-03-14 21:48:28 +0800

    Use followed_posts (fixed the query)

--- src/flask_learning/app/models.py
+++ src/flask_learning/app/models.py
@@ -87,11 +87,9 @@ class User(UserMixin, DbModel):
         )
 
     def followed_posts(self) -> BaseQuery:
-        followed = (
-            Post.query.join(followers, followers.c.followed_id == Post.user_id)
-            .filter(followers.c.follower_id == self.id)
-            .order_by(Post.timestamp.desc())
-        )
+        followed = Post.query.join(
+            followers, followers.c.followed_id == Post.user_id
+        ).filter(followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
