67677643ce3aa6fbd1a58ed34363bdda75e78e2d
Derek Wan <d.wan@icloud.com>
2021-04-11 11:36:42 +0800

    Add link to user in post.html, and annotate not_found_error

--- src/flask_learning/app/errors.py
+++ src/flask_learning/app/errors.py
@@ -1,19 +1,19 @@
 from typing import Any
 
 from flask import render_template
+from werkzeug.exceptions import NotFound
 
 from flask_learning.app import app
 from flask_learning.app import db_session
 
 
 @app.errorhandler(404)
-def not_found_error(error: Any) -> tuple[Any, int]:
-    raise NotImplementedError(type(error), render_template("404.html"))
+def not_found_error(error: NotFound) -> tuple[str, int]:  # noqa: U100
     return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_error(error: Any) -> tuple[Any, int]:
-    raise NotImplementedError(type(error), render_template("404.html"))
+    raise NotImplementedError(type(error), render_template("500.html"))
     db_session.rollback()
     return render_template("500.html"), 500
